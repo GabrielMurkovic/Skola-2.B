ba82da477eec961d1ca164680832124d40d9674a
GabrielMurkovic <murkovicgabriel@gmail.com>
2026-09-21 10:21:52 +0200

    Add files via upload

---
+++ Turnir_LV1_MalaDorada.cpp
@@ -0,0 +1,32 @@
+// Naziv Funckije:Prolazi
+// Ulazni parametri: broj bodova u 1., 2. i 3. zadatku
+// Povratna vrijednost: true ili false
+// Što radi main(), a što funkcija?  Main učitava bodove i ispisuje dali prolazi ili ne,a funkcija provjerava zadoljava li natjecatelj sve uvjete
+
+#include <iostream>
+using namespace std;
+
+bool prolazi(int prvi, int drugi, int treci) {
+	if ((prvi + drugi + treci) >= 180 && prvi >= 40 && drugi >= 40 && treci >= 40) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+
+
+
+int main() {
+	int prvi, drugi, treci;
+	cin >> prvi >> drugi >> treci;
+
+	if (prolazi(prvi, drugi, treci)) {
+		cout << "PROLAZI";
+	}
+	else {
+		cout << "NE PROLAZI";
+	}
+}
